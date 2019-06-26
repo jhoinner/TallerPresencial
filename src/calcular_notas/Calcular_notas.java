125ea0f9a7116ac7ee9deceb34e0b769c3c7f32e
JHOINNER <jhoinnerballestas@gmail.com>
2019-06-26 13:25:43 -0500

    primer commit

---
+++ src/calcular_notas/Calcular_notas.java
@@ -0,0 +1,28 @@
+
+package calcular_notas;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jhoinner Constante.
+ */
+public class Calcular_notas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int cant,x;
+       Scanner leer = new Scanner(System.in);
+       ArrayList<Integer>Estu = null;
+       System.out.println("Digite la cantidad de estudiantes");
+       cant = leer.nextInt();
+       for(x=1; x<=cant; x++){
+           
+       }
+       
+    }
+    
+}
