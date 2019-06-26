48cf38762c448bd1326a6819af16bdf357cca0d4
JHOINNER <jhoinnerballestas@gmail.com>
2019-06-26 13:53:46 -0500

    (problema solucionado

--- src/calcular_notas/Calcular_notas.java
+++ src/calcular_notas/Calcular_notas.java
@@ -18,15 +18,21 @@ public class Calcular_notas {
        Scanner leer = new Scanner(System.in);
        ArrayList <String> Produc;
        Produc = new ArrayList();
+       ArrayList <Double> prom;
+       prom = new ArrayList();
        System.out.println("Digite la cantidad de estudiantes");
        cant = leer.nextInt();
        for(x=1; x<=cant; x++){
           System.out.println("Digite nombre del estudiante"); 
           Produc.add(leer.next());
+          System.out.println("digite promerio del estudiante");
+          prom.add(leer.nextDouble());
        }
        for(i=0; i<Produc.size(); i++){
-           System.out.println(Produc.get(i));
-       }       
+           System.out.println(Produc.get(i)+"\t"+", Promedio: "+prom.get(i));
+           
+       }
+       
     }
     
 }
