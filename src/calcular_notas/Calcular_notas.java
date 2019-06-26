b1c4787f66455374378152db4d3499f66d2aa6ad
JHOINNER <jhoinnerballestas@gmail.com>
2019-06-26 14:32:34 -0500

    hasta aqui llegue

--- src/calcular_notas/Calcular_notas.java
+++ src/calcular_notas/Calcular_notas.java
@@ -14,26 +14,26 @@ public class Calcular_notas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int cant,x,i;
+        int cant,x,i,f;
+        double prom;
        Scanner leer = new Scanner(System.in);
        ArrayList <String> Produc;
        Produc = new ArrayList();
-       ArrayList <Double> prom;
-       prom = new ArrayList();
        System.out.println("Digite la cantidad de estudiantes");
        cant = leer.nextInt();
        for(x=1; x<=cant; x++){
           System.out.println("Digite nombre del estudiante"); 
           Produc.add(leer.next());
+       }
+       for(f=1; f<=cant; f++){
           System.out.println("digite promerio del estudiante");
-          prom.add(leer.nextDouble());
+          
        }
        for(i=0; i<Produc.size(); i++){
-           System.out.println(Produc.get(i)+"\t"+", Promedio: "+prom.get(i));
-           if((i)>3.0){
-               System.out.println((i));
-           }
+           System.out.println(Produc.get(i));
+           
        }
+       
        
        
        
